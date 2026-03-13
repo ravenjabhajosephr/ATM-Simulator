c423065793ed8d1dc866974debc3881b1c9a551e
ravenjabhajosephr <ravenjabhajosephr@gmail.com>
2026-03-13 14:07:17 +0100

    scanf debugged and bool lib for bool check.

--- main.c
+++ main.c
@@ -1,7 +1,40 @@
 #include <stdio.h>
+#include <stdbool.h>
 
 int main(void) {
-    printf("ATM simulator initialised\n");
+    float balance = 1000.00;
+    int choice = 0;
+    bool loop = true;
+    printf("-------Welcome to C-bank-------\n");
 
-    return 0;
-}
+    while(loop){
+        printf("Select one of the below options:\n");
+        printf("1. Check current balance\n2. Deposit\n3. Withdraw\n4. Exit\n");
+        printf("Enter value here: "); 
+        
+        //Removed \n and stored result in memory address of variable choice
+        scanf("%d", &choice);
+    
+            if (choice == 4) { 
+                loop = false;    
+            }
+            else if (choice == 1) {
+                printf("Enter current balance:\n");
+            }
+            else if (choice == 2) {
+                printf("Enter deposit amount:\n");
+            }
+            else if (choice == 3) {
+                printf("Enter withdraw amount:\n");
+            }
+            else {
+                printf("Invalid option");
+            }
+        
+        }
+    
+        printf("Thank you for using our bank");
+        return 0;
+    }
+
+    
